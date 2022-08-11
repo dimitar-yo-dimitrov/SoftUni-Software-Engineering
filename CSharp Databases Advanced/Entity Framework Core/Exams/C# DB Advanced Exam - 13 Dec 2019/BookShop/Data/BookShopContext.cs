d89709b7321f6385bdeea0f2a5b6b500236d2e93
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2022-08-11 15:21:59 +0300

    Add exam

---
+++ CSharp Databases Advanced/Entity Framework Core/Exams/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/BookShopContext.cs
@@ -0,0 +1,44 @@
+﻿using BookShop.Data.Models;
+
+namespace BookShop.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class BookShopContext : DbContext
+    {
+        public BookShopContext() { }
+
+        public BookShopContext(DbContextOptions options)
+            : base(options) { }
+
+        public virtual DbSet<Author> Authors { get; set; }
+
+        public virtual DbSet<AuthorBook> AuthorsBooks { get; set; }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+
+            optionsBuilder
+                .UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuthorBook>(a =>
+            {
+                a.HasKey(ab => new
+                {
+                    ab.AuthorId,
+                    ab.BookId
+                });
+            });
+        }
+    }
+}
