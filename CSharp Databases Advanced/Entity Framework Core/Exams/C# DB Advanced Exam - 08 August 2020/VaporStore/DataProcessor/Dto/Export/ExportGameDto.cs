95ad424a1ede20d72e42251d80596a8d5523f0ad
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2022-08-14 16:10:10 +0300

    Fix XML Property

--- CSharp Databases Advanced/Entity Framework Core/Exams/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Dto/Export/ExportGameDto.cs
+++ CSharp Databases Advanced/Entity Framework Core/Exams/C# DB Advanced Exam - 08 August 2020/VaporStore/DataProcessor/Dto/Export/ExportGameDto.cs
@@ -10,13 +10,10 @@ namespace VaporStore.DataProcessor.Dto.Export
         [XmlAttribute("title")]
         public string Title { get; set; }
 
-        [Required]
-        [JsonProperty("Genre")]
+        [XmlElement("Genre")]
         public string Genre { get; set; }
 
-        [Required]
-        [Range(typeof(decimal), "0.00", "79228162514264337593543950335")]
-        [JsonProperty("Price")]
+        [XmlElement("Price")]
         public decimal Price { get; set; }
     }
 }
