484a06488d3df8fd0e48a3bfe8031da95274a799
Dimitar Dimitrov <dimitar.dimitrov.yo@gmail.com>
2022-10-10 10:18:48 +0300

    Update Configuration.cs

--- CSharp Databases Advanced/Entity Framework Core/Homeworks/05.LINQ/MusicHub/Data/Configuration.cs
+++ CSharp Databases Advanced/Entity Framework Core/Homeworks/05.LINQ/MusicHub/Data/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public static string ConnectionString =
-            @"Server=DESKTOP-B9BOKON\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+            @"";
     }
 }
