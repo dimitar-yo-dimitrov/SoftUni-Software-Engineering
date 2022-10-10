4c46b59a4ff1ec8086c4be5b91319bc78d77fe1b
Dimitar Dimitrov <dimitar.dimitrov.yo@gmail.com>
2022-10-10 10:08:30 +0300

    Update Configuration.cs

--- CSharp Databases Advanced/Entity Framework Core/Exams/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Configuration.cs
+++ CSharp Databases Advanced/Entity Framework Core/Exams/C# DB Advanced Exam - 08 August 2020/VaporStore/Data/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public static string ConnectionString =
-            @"Server=DESKTOP-B9BOKON\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
+            @"";
     }
 }
