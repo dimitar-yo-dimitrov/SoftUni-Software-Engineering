19624e96a60ccf4f21707446f87345ef2e05dfe5
Dimitar Dimitrov <dimitar.dimitrov.yo@gmail.com>
2022-10-10 10:09:33 +0300

    Update Configuration.cs

--- CSharp Databases Advanced/Entity Framework Core/Exams/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Configuration.cs
+++ CSharp Databases Advanced/Entity Framework Core/Exams/C# DB Advanced Retake Exam - 16 Dec 2021/Artillery/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=DESKTOP-B9BOKON\SQLEXPRESS;Database=Artillery;Trusted_Connection=True";
+        public static string ConnectionString = @"";
     }
 }
